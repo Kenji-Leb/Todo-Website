562f50cab090c5f0086e3439714c37e8d719856d
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-02-29 21:35:02 +0200

    finished the login page js

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
-let username = document.getElementById('username').value;
-let password = document.getElementById('password').value;
-
 function loginVerify(){
-	if (username == 'AdminSEF123' && 
-        password == 'SeF@ctORy$$456') {
-        alert('Login successful!')}
-    else{
-        alert('incorrect')
-	}
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    if (username == 'AdminSEF123' && password == 'SeF@ctORy$$456') {
+        window.location.href = "./TDL-page.html";
+    } else {
+        alert('Incorrect username or password. Please try again.');
+    }
 }
+
+
